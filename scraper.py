4fe9ec42dc25530b5e10fa6ed855b573790bba7d
coolsunwind <brianleung1017@gmail.com>
2018-07-12 12:51:54 +0800

    Add files via upload

--- scraper.py
+++ scraper.py
@@ -1,21 +1,10 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
 import requests
 from bs4 import BeautifulSoup
 import re
 from datetime import datetime
 from datetime import timedelta
 import pytz
-import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
+#import scraperwiki
 
 def queryHourlyRainfall(y, m, d, h):
     r = requests.get(
@@ -64,22 +53,11 @@ ytdY = ytd.strftime("%y")
 # print(ytd.strftime("%m"))
 # print(ytd.strftime("%d"))
 
-#print(queryDailyRainfall(ytd.strftime("%y"), ytd.strftime("%m"), ytd.strftime("%d")))
-daily = queryDailyRainfall("18", "06", "05")
+daily = print(queryDailyRainfall(ytd.strftime("%y"), ytd.strftime("%m"), ytd.strftime("%d")))
+#daily = queryDailyRainfall("18", "06", "05")
 for hour in daily:
     for h in hour:
         h['key'] = '{}{}{}{}-{}'.format(ytdY, h['month'],h['day'],h['hour'],h['region'])
         scraperwiki.sqlite.save(unique_keys=['key'], data=h)
         print(h)
-
-# # Write out to the sqlite database using scraperwiki library
-#scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
-
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+print(ytd)
